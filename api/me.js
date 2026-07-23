138e5373d722210054185be2e0f8850f14adafa9
bigmanjtech <bigmanj429@gmail.com>
2026-07-23 10:02:46 +0300

    Update me.js

--- api/me.js
+++ api/me.js
@@ -1,18 +1,13 @@
-// api/me.js
-// This file returns the logged-in user's profile data
-
 import { PrismaClient } from '@prisma/client';
 import authMiddleware from '../middleware/auth.js';
 
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  // Only allow GET requests
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  // Run the auth middleware to check if user is logged in
   await new Promise((resolve, reject) => {
     authMiddleware(req, res, (err) => {
       if (err) reject(err);
@@ -20,11 +15,9 @@ export default async function handler(req, res) {
     });
   });
 
-  // If auth failed, it would have returned a response already
   const userId = req.user.id;
 
   try {
-    // Fetch the user from the database
     const user = await prisma.user.findUnique({
       where: { id: userId },
       select: {
@@ -42,7 +35,6 @@ export default async function handler(req, res) {
       return res.status(404).json({ error: 'User not found' });
     }
 
-    // Return the user profile
     res.status(200).json({
       success: true,
       user: user
